44b1b4bfa35070d0e6bbccdc348a3c1eb109e6c8
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2011-09-23 10:09:32 +0200

    first commit

---
+++ randomproject/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomproject;
+/**
+ *
+ * @author Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
